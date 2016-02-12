ad15364018189b4faec8c25d80033c1b856210bf
Fabian Peters <fabian@mercadu.de>
2016-02-12 17:20:52 +0000

    First cut of a popup with filter choices.

--- Frameworks/D2W/ERModernDirectToWeb/Sources/er/modern/directtoweb/components/query/ERMD2WListFilter.java
+++ Frameworks/D2W/ERModernDirectToWeb/Sources/er/modern/directtoweb/components/query/ERMD2WListFilter.java
@@ -6,9 +6,15 @@ import com.webobjects.appserver.WOResponse;
 import com.webobjects.eoaccess.EODatabaseDataSource;
 import com.webobjects.eocontrol.EODataSource;
 import com.webobjects.eocontrol.EOQualifier;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSKeyValueCoding;
+import com.webobjects.foundation.NSMutableArray;
 
 import er.ajax.AjaxUtils;
 import er.directtoweb.components.ERDCustomQueryComponent;
+import er.extensions.eof.ERXQ;
+import er.extensions.foundation.ERXArrayUtilities;
+import er.extensions.foundation.ERXStringUtilities;
 import er.modern.directtoweb.delegates.ERMD2WAttributeQueryDelegate;
 import er.modern.directtoweb.delegates.ERMD2WAttributeQueryDelegate.ERMD2WQueryComponent;
 
@@ -28,11 +34,18 @@ public class ERMD2WListFilter extends ERDCustomQueryComponent implements
     private static final long serialVersionUID = 1L;
     
     public interface Keys extends ERDCustomQueryComponent.Keys {
+        public static final String searchChoices = "searchChoices";
+        public static final String searchChoicesDisplayKey = "searchChoicesDisplayKey";
+        public static final String searchChoicesKey = "searchChoicesKey";
+        public static final String searchChoicesRecursionKey = "searchChoicesRecursionKey";
         public static final String searchKey = "searchKey";
         public static final String typeAheadMinimumCharacterCount = "typeAheadMinimumCharacterCount";
     }
 
+    private Object _searchChoice;
     private String _searchValue;
+
+    public Object searchChoiceItem;
 
     public ERMD2WListFilter(WOContext context) {
         super(context);
@@ -44,10 +57,28 @@ public class ERMD2WListFilter extends ERDCustomQueryComponent implements
     }
 
     // actions
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public WOActionResults search() {
         EODataSource dataSource = displayGroup().dataSource();
         EOQualifier _qualifier = ERMD2WAttributeQueryDelegate.instance
                 .buildQualifier(this);
+        
+        String searchChoicesKey = (String) d2wContext().valueForKey(Keys.searchChoicesKey);
+        if (!ERXStringUtilities.stringIsNullOrEmpty(searchChoicesKey)
+                && searchChoice() != null) {
+            if (d2wContext().valueForKey(Keys.searchChoicesRecursionKey) != null) {
+                String recursionKey = (String) d2wContext()
+                        .valueForKey(Keys.searchChoicesRecursionKey);
+                NSMutableArray deepChoices = new NSMutableArray(searchChoice());
+                deepChoices.addObjects(NSKeyValueCoding.Utility
+                        .valueForKey(searchChoice(), recursionKey));
+                _qualifier = ERXQ.and(_qualifier, ERXQ.in(searchChoicesKey, ERXArrayUtilities.flatten(deepChoices)));
+            } else {
+                _qualifier = ERXQ.and(_qualifier,
+                        ERXQ.equals(searchChoicesKey, searchChoice()));
+            }
+        }
+        
         ((EODatabaseDataSource) dataSource).setAuxiliaryQualifier(_qualifier);
         ((EODatabaseDataSource) displayGroup().dataSource()).fetchSpecification()
                 .setUsesDistinct(true);
@@ -68,6 +99,30 @@ public class ERMD2WListFilter extends ERDCustomQueryComponent implements
     @Override
     public String searchValue() {
         return _searchValue;
+    }
+
+    public void setSearchChoice(Object searchChoice) {
+        _searchChoice = searchChoice;
+    }
+
+    public Object searchChoice() {
+        return _searchChoice;
+    }
+    
+    public String searchChoicesDisplayString() {
+        String displayKey  = (String) d2wContext().valueForKey(Keys.searchChoicesDisplayKey);
+        return (String) NSKeyValueCoding.Utility.valueForKey(searchChoiceItem, displayKey);
+    }
+
+    @SuppressWarnings({ "rawtypes" })
+    public NSArray searchChoices() {
+        NSArray searchChoices = NSArray.emptyArray();
+        if (d2wContext().valueForKey(Keys.searchChoices) != null) {
+            String searchChoicesKey = (String) d2wContext()
+                    .valueForKey(Keys.searchChoices);
+            searchChoices = (NSArray) valueForKeyPath(searchChoicesKey);
+        }
+        return searchChoices;
     }
 
     @Override
