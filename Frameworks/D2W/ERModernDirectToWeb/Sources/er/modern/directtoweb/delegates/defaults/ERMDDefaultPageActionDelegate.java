8592687309d4400110072c6658f9dde5493c5488
Jürgen Tabert <jtabert@behemoth.local>
2016-04-08 02:36:56 +0200

    dirty trick to switch between delegate and default page buttons

--- Frameworks/D2W/ERModernDirectToWeb/Sources/er/modern/directtoweb/delegates/defaults/ERMDDefaultPageActionDelegate.java
+++ Frameworks/D2W/ERModernDirectToWeb/Sources/er/modern/directtoweb/delegates/defaults/ERMDDefaultPageActionDelegate.java
@@ -21,6 +21,7 @@ import com.webobjects.eocontrol.EOEnterpriseObject;
 import com.webobjects.foundation.NSKeyValueCoding;
 import com.webobjects.foundation.NSValidation;
 
+import er.ajax.AjaxUpdateContainer;
 import er.directtoweb.delegates.ERDBranchDelegate;
 import er.directtoweb.delegates.ERDBranchInterface;
 import er.directtoweb.pages.ERD2WInspectPage;
@@ -30,48 +31,61 @@ import er.extensions.eof.ERXEOAccessUtilities;
 import er.extensions.eof.ERXGenericRecord;
 import er.extensions.foundation.ERXValueUtilities;
 import er.extensions.localization.ERXLocalizer;
-import er.modern.directtoweb.components.buttons.ERMDPageActionControllerButton;
 
 public class ERMDDefaultPageActionDelegate extends ERDBranchDelegate {
-	
+
 	/**
-	 * Do I need to update serialVersionUID?
-	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
-	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+	 * Do I need to update serialVersionUID? See section 5.6 <cite>Type Changes
+	 * Affecting Serialization</cite> on page 51 of the
+	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object
+	 * Serialization Spec</a>
 	 */
 	private static final long serialVersionUID = 1L;
 
 	private static final Logger log = Logger.getLogger(ERMDDefaultPageActionDelegate.class);
-	
-	public boolean shouldSaveChanges(D2WContext c) { return ERXValueUtilities.booleanValue(c.valueForKey("shouldSaveChanges")); }
-	public boolean shouldValidateBeforeSave(D2WContext c) { return ERXValueUtilities.booleanValue(c.valueForKey("shouldValidateBeforeSave")); }
-	public boolean shouldRecoverFromOptimisticLockingFailure(D2WContext c) { return ERXValueUtilities.booleanValueWithDefault(c.valueForKey("shouldRecoverFromOptimisticLockingFailure"), false); }
-	public boolean shouldRevertUponSaveFailure(D2WContext c) { return ERXValueUtilities.booleanValueWithDefault(c.valueForKey("shouldRevertUponSaveFailure"), false); }
-	
+
+	public boolean shouldSaveChanges(D2WContext c) {
+		return ERXValueUtilities.booleanValue(c.valueForKey("shouldSaveChanges"));
+	}
+
+	public boolean shouldValidateBeforeSave(D2WContext c) {
+		return ERXValueUtilities.booleanValue(c.valueForKey("shouldValidateBeforeSave"));
+	}
+
+	public boolean shouldRecoverFromOptimisticLockingFailure(D2WContext c) {
+		return ERXValueUtilities.booleanValueWithDefault(c.valueForKey("shouldRecoverFromOptimisticLockingFailure"), false);
+	}
+
+	public boolean shouldRevertUponSaveFailure(D2WContext c) {
+		return ERXValueUtilities.booleanValueWithDefault(c.valueForKey("shouldRevertUponSaveFailure"), false);
+	}
+
 	public WOComponent _save(WOComponent sender) {
 		WOComponent nextPage = sender.context().page();
 		EOEnterpriseObject eo = object(sender);
 		D2WContext c = d2wContext(sender);
-		ERD2WPage page = (ERD2WPage)ERD2WUtilities.enclosingComponentOfClass(sender, InspectPageInterface.class);
-		
-		if(eo != null && eo.editingContext() == null) {
+		ERD2WPage page = (ERD2WPage) ERD2WUtilities.enclosingComponentOfClass(sender, InspectPageInterface.class);
+
+		if (eo != null && eo.editingContext() == null) {
 			page.setErrorMessage(ERXLocalizer.currentLocalizer().localizedTemplateStringForKeyWithObject("ERD2WInspect.alreadyAborted", c));
 			page.clearValidationFailed();
 			return nextPage;
 		}
 
-		if(!page.errorMessages().isEmpty()) {
+		if (!page.errorMessages().isEmpty()) {
 			page.setErrorMessage(null);
 			return nextPage;
 		}
-		
-		if(eo == null) { return _nextPageFromDelegate(page); }
-		
+
+		if (eo == null) {
+			return _nextPageFromDelegate(page);
+		}
+
 		boolean shouldRevert = false;
 		EOEditingContext ec = eo.editingContext();
 		ec.lock();
 		try {
-			if(shouldValidateBeforeSave(c)) {
+			if (shouldValidateBeforeSave(c)) {
 				if (ec.insertedObjects().containsObject(eo)) {
 					eo.validateForInsert();
 				} else {
@@ -79,24 +93,26 @@ public class ERMDDefaultPageActionDelegate extends ERDBranchDelegate {
 				}
 			}
 			boolean hasChanges = ec.hasChanges();
-			if(shouldSaveChanges(c) && hasChanges) {
+			if (shouldSaveChanges(c) && hasChanges) {
 				try {
 					ec.saveChanges();
 					nextPage = _nextPageFromDelegate(page);
 					// Refresh object to update derived attributes
 					ec.refreshObject(eo);
-				} catch(RuntimeException e) {
-					if(shouldRevertUponSaveFailure(c)) { shouldRevert = true; }
+				} catch (RuntimeException e) {
+					if (shouldRevertUponSaveFailure(c)) {
+						shouldRevert = true;
+					}
 					throw e;
 				}
-			} else if(!hasChanges) {
+			} else if (!hasChanges) {
 				nextPage = _nextPageFromDelegate(page);
 			}
-		} catch(NSValidation.ValidationException e) {
+		} catch (NSValidation.ValidationException e) {
 			page.setErrorMessage(ERXLocalizer.currentLocalizer().localizedTemplateStringForKeyWithObject("CouldNotSave", e));
 			page.validationFailedWithException(e, e.object(), "saveChangesExceptionKey");
-		} catch(EOGeneralAdaptorException e) {
-			if(ERXEOAccessUtilities.isOptimisticLockingFailure(e) && shouldRecoverFromOptimisticLockingFailure(c)) {
+		} catch (EOGeneralAdaptorException e) {
+			if (ERXEOAccessUtilities.isOptimisticLockingFailure(e) && shouldRecoverFromOptimisticLockingFailure(c)) {
 				EOEnterpriseObject obj = ERXEOAccessUtilities.refetchFailedObject(ec, e);
 				page.setErrorMessage(ERXLocalizer.currentLocalizer().localizedTemplateStringForKeyWithObject("CouldNotSavePleaseReapply", c));
 				page.validationFailedWithException(e, obj, "CouldNotSavePleaseReapply");
@@ -105,37 +121,42 @@ public class ERMDDefaultPageActionDelegate extends ERDBranchDelegate {
 			}
 		} finally {
 			try {
-				if(shouldRevert) { ec.revert(); }
+				if (shouldRevert) {
+					ec.revert();
+				}
 			} finally {
 				ec.unlock();
 			}
 		}
-		
+		//String id = (String) page.d2wContext().valueForKey("idForParentMainContainer");
+		//AjaxUpdateContainer.updateContainerWithID(id, sender.context());
 		return nextPage;
 	}
-	
-    public WOComponent _cancelEdit(WOComponent sender) {
+
+	public WOComponent _cancelEdit(WOComponent sender) {
 		EOEnterpriseObject eo = object(sender);
 		ERD2WInspectPage page = ERD2WUtilities.enclosingComponentOfClass(sender, ERD2WInspectPage.class);
-		EOEditingContext ec = eo != null?eo.editingContext():null;
+		//String id = (String) page.d2wContext().valueForKey("idForParentMainContainer");
+		//AjaxUpdateContainer.updateContainerWithID(id, sender.context());
+		EOEditingContext ec = eo != null ? eo.editingContext() : null;
 		if (ec != null && page.shouldRevertChanges()) {
-            ec.revert();
-        }
-        return page.nextPage(false);
+			ec.revert();
+		}
+		return page.nextPage(false);
 	}
-	
+
 	public void _createRelated(WOComponent sender) {
 		EditRelationshipPageInterface erpi = ERD2WUtilities.enclosingComponentOfClass(sender, EditRelationshipPageInterface.class);
-		if(erpi != null) {
-			EOEnterpriseObject eo = (EOEnterpriseObject)NSKeyValueCoding.Utility.valueForKey(erpi, "masterObject");
-			String relationshipKey = (String)NSKeyValueCoding.Utility.valueForKey(erpi, "relationshipKey");
-			if(!ERXValueUtilities.isNull(eo) && !StringUtils.isBlank(relationshipKey)) {
+		if (erpi != null) {
+			EOEnterpriseObject eo = (EOEnterpriseObject) NSKeyValueCoding.Utility.valueForKey(erpi, "masterObject");
+			String relationshipKey = (String) NSKeyValueCoding.Utility.valueForKey(erpi, "relationshipKey");
+			if (!ERXValueUtilities.isNull(eo) && !StringUtils.isBlank(relationshipKey)) {
 				EOEditingContext nestedEC = ERXEC.newEditingContext(eo.editingContext());
 				EOClassDescription relatedObjectClassDescription = eo.classDescriptionForDestinationKey(relationshipKey);
-				EOEnterpriseObject relatedObject = (EOEnterpriseObject)EOUtilities.createAndInsertInstance(nestedEC, relatedObjectClassDescription.entityName());
+				EOEnterpriseObject relatedObject = (EOEnterpriseObject) EOUtilities.createAndInsertInstance(nestedEC, relatedObjectClassDescription.entityName());
 				EOEnterpriseObject localObj = EOUtilities.localInstanceOfObject(relatedObject.editingContext(), eo);
 				if (localObj instanceof ERXGenericRecord) {
-					((ERXGenericRecord)localObj).setValidatedWhenNested(false);
+					((ERXGenericRecord) localObj).setValidatedWhenNested(false);
 				}
 				localObj.addObjectToBothSidesOfRelationshipWithKey(relatedObject, relationshipKey);
 				NSKeyValueCoding.Utility.takeValueForKey(erpi, relatedObject, "selectedObject");
@@ -143,84 +164,75 @@ public class ERMDDefaultPageActionDelegate extends ERDBranchDelegate {
 			}
 		}
 	}
-	
-    public void _queryRelated(WOComponent sender) {
-        EditRelationshipPageInterface erpi = ERD2WUtilities
-                .enclosingComponentOfClass(sender, EditRelationshipPageInterface.class);
-        if (erpi != null) {
-            D2WContext c = (D2WContext) NSKeyValueCoding.Utility.valueForKey(erpi, "d2wContext");
-            if (ERXValueUtilities.booleanValueWithDefault(
-                    c.valueForKey("shouldShowQueryPage"), true)) {
-                NSKeyValueCoding.Utility.takeValueForKey(erpi, "query",
-                        "inlineTaskSafely");
-            } else {
-                // go straight to the select page
-                EOEnterpriseObject eo = (EOEnterpriseObject) NSKeyValueCoding.Utility
-                        .valueForKey(erpi, "masterObject");
-                EOEditingContext ec = eo.editingContext();
-                String relationshipKey = (String) NSKeyValueCoding.Utility
-                        .valueForKey(erpi, "relationshipKey");
-                if(!ERXValueUtilities.isNull(eo) && !StringUtils.isBlank(relationshipKey)) {
-                    String destinationEntityName = eo
-                            .classDescriptionForDestinationKey(relationshipKey)
-                            .entityName();
-                    EODatabaseDataSource ds = new EODatabaseDataSource(ec, destinationEntityName);
-                    NSKeyValueCoding.Utility.takeValueForKey(erpi, ds,
-                            "selectDataSource");
-                    NSKeyValueCoding.Utility.takeValueForKey(erpi, "list",
-                            "inlineTaskSafely");
-                }
-            }
-        }
-    }
-	
+
+	public void _queryRelated(WOComponent sender) {
+		EditRelationshipPageInterface erpi = ERD2WUtilities.enclosingComponentOfClass(sender, EditRelationshipPageInterface.class);
+		if (erpi != null) {
+			D2WContext c = (D2WContext) NSKeyValueCoding.Utility.valueForKey(erpi, "d2wContext");
+			if (ERXValueUtilities.booleanValueWithDefault(c.valueForKey("shouldShowQueryPage"), true)) {
+				NSKeyValueCoding.Utility.takeValueForKey(erpi, "query", "inlineTaskSafely");
+			} else {
+				// go straight to the select page
+				EOEnterpriseObject eo = (EOEnterpriseObject) NSKeyValueCoding.Utility.valueForKey(erpi, "masterObject");
+				EOEditingContext ec = eo.editingContext();
+				String relationshipKey = (String) NSKeyValueCoding.Utility.valueForKey(erpi, "relationshipKey");
+				if (!ERXValueUtilities.isNull(eo) && !StringUtils.isBlank(relationshipKey)) {
+					String destinationEntityName = eo.classDescriptionForDestinationKey(relationshipKey).entityName();
+					EODatabaseDataSource ds = new EODatabaseDataSource(ec, destinationEntityName);
+					NSKeyValueCoding.Utility.takeValueForKey(erpi, ds, "selectDataSource");
+					NSKeyValueCoding.Utility.takeValueForKey(erpi, "list", "inlineTaskSafely");
+				}
+			}
+		}
+	}
+
 	public WOComponent _returnRelated(WOComponent sender) {
 		WOComponent nextPage = null;
 		EditRelationshipPageInterface erpi = ERD2WUtilities.enclosingComponentOfClass(sender, EditRelationshipPageInterface.class);
-		if(erpi != null) {
-			EOEnterpriseObject eo = (EOEnterpriseObject)NSKeyValueCoding.Utility.valueForKey(erpi, "masterObject");
-			if(eo != null) {
-				//eo should be in a non-validating nested ec so no exceptions should throw
+		if (erpi != null) {
+			EOEnterpriseObject eo = (EOEnterpriseObject) NSKeyValueCoding.Utility.valueForKey(erpi, "masterObject");
+			if (eo != null) {
+				// eo should be in a non-validating nested ec so no exceptions
+				// should throw
 				eo.editingContext().saveChanges();
 			}
-			if(erpi instanceof D2WPage) {
-				nextPage = _nextPageFromDelegate((D2WPage)erpi);
+			if (erpi instanceof D2WPage) {
+				nextPage = _nextPageFromDelegate((D2WPage) erpi);
 			}
 			if (nextPage == null && eo != null) {
-				nextPage = (WOComponent)D2W.factory().editPageForEntityNamed(eo.entityName(), sender.session());
-				((EditPageInterface)nextPage).setObject(eo);
+				nextPage = (WOComponent) D2W.factory().editPageForEntityNamed(eo.entityName(), sender.session());
+				((EditPageInterface) nextPage).setObject(eo);
 			}
 		} else {
 			nextPage = _return(sender);
 		}
 		return nextPage;
 	}
-	
+
 	public WOComponent _return(WOComponent sender) {
 		D2WPage page = ERD2WUtilities.enclosingComponentOfClass(sender, D2WPage.class);
 		WOComponent nextPage = _nextPageFromDelegate(page);
-		if(nextPage == null) {
+		if (nextPage == null) {
 			nextPage = D2W.factory().defaultPage(sender.session());
 		}
 		return nextPage;
 	}
-	
+
 	protected WOComponent _nextPageFromDelegate(D2WPage page) {
 		WOComponent nextPage = null;
 		NextPageDelegate delegate = page.nextPageDelegate();
-		if(delegate != null) {
-			if (!((delegate instanceof ERDBranchDelegate) && (((ERDBranchInterface)page).branchName() == null))) {
-				/* 
-				 * we assume here, because nextPage() in ERDBranchDelegate 
-				 * is final, we can't do something reasonable when none of 
-				 * the branch buttons was selected. This allows us to throw 
-				 * a branch delegate at any page, even when no branch was 
-				 * taken
+		if (delegate != null) {
+			if (!((delegate instanceof ERDBranchDelegate) && (((ERDBranchInterface) page).branchName() == null))) {
+				/*
+				 * we assume here, because nextPage() in ERDBranchDelegate is
+				 * final, we can't do something reasonable when none of the
+				 * branch buttons was selected. This allows us to throw a branch
+				 * delegate at any page, even when no branch was taken
 				 */
 				nextPage = delegate.nextPage(page);
 			}
 		}
-		if(nextPage == null) {
+		if (nextPage == null) {
 			nextPage = page.nextPage();
 		}
 		return nextPage;
