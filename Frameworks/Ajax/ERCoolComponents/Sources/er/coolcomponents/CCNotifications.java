4283c8d3dcb90e8d5a0abfc82ae77a4130c87b9e
Fabian Peters <fabian@mercadu.de>
2016-10-07 15:40:22 +0200

    Make CCNotifications work with both full page loads and ajax requests. To achieve this, we use a simple notification queue based on the session object store. Three notification types are supported: "success", "error" and "alert", the latter represents a modal dialogue on which the user has to click to move on.
    TODO: Implement filtering of notifications by key path and value, to enable restricted display of notifications pushed out via NSNotification (e.g. pass a UUID and a key path "session.user.guid" to restrict display of the notification to sessions of a specific user.

--- Frameworks/Ajax/ERCoolComponents/Sources/er/coolcomponents/CCNotifications.java
+++ Frameworks/Ajax/ERCoolComponents/Sources/er/coolcomponents/CCNotifications.java
@@ -1,15 +1,21 @@
 package er.coolcomponents;
 
-import com.webobjects.appserver.WOComponent;
+import java.util.UUID;
+
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSNotification;
 import com.webobjects.foundation.NSNotificationCenter;
 import com.webobjects.foundation.NSSelector;
+import com.webobjects.foundation.NSTimestamp;
 
 import er.ajax.AjaxUtils;
+import er.extensions.appserver.ERXSession;
 import er.extensions.appserver.ERXWOContext;
+import er.extensions.components.ERXComponent;
 import er.extensions.eof.ERXConstant;
-import er.extensions.foundation.ERXStringUtilities;
 
 /**
  * Displays one or more notifications. This is basically a thin wrapper around
@@ -19,11 +25,17 @@ import er.extensions.foundation.ERXStringUtilities;
  * 
  * @author fpeters
  */
-public class CCNotifications extends WOComponent {
+public class CCNotifications extends ERXComponent {
 
     private static final long serialVersionUID = 1L;
 
-    private static final String SHOW_UI_NOTIFICATION = "ShowUiNotification";
+    private static final String CC_NOTIFICATION = "CCNotification";
+
+    public static enum TYPE {
+        alert, error, success
+    };
+
+    public NSDictionary<String, Object> aNotification;
 
     public CCNotifications(WOContext aContext) {
         super(aContext);
@@ -34,50 +46,113 @@ public class CCNotifications extends WOComponent {
         super.awake();
         NSNotificationCenter.defaultCenter()
                 .addObserver(this,
-                        new NSSelector<Void>("displayNotification",
+                        new NSSelector<Void>("publishNSNotification",
                                 ERXConstant.NotificationClassArray),
-                        SHOW_UI_NOTIFICATION, null);
+                        CC_NOTIFICATION, null);
     }
 
     @Override
     public void sleep() {
-        NSNotificationCenter.defaultCenter().removeObserver(this, SHOW_UI_NOTIFICATION,
+        NSNotificationCenter.defaultCenter().removeObserver(this, CC_NOTIFICATION,
                 null);
         super.sleep();
     }
 
     /**
-     * Extracts a message from an NSNotification's object. The user info dictionary may
-     * contain a "type" key, to set the notification type to pass to alertify
-     * (e.g. "success", "error").
+     * Extracts a message from an NSNotification's object. The user info
+     * dictionary may contain a "type" key, set to one of the valid notification
+     * types.
      * 
      * @param notification
      */
-    public void displayNotification(NSNotification notification) {
-        String notificationType = "success";
+    public void publishNSNotification(NSNotification notification) {
+        TYPE notificationType = TYPE.success;
         if (notification.userInfo() != null
                 && notification.userInfo().containsKey("type")) {
-            notificationType = (String) notification.userInfo().valueForKey("type");
+            notificationType = (TYPE) notification.userInfo().valueForKey("type");
         }
-        AjaxUtils.javascriptResponse(
-                "alertify." + notificationType + "('" + notification.object() + "')",
-                context());
+        notify((String) notification.object(), notificationType);
     }
 
-    public static void displayNotification(String message) {
-        displaySuccessNotification(message);
+    public static void notify(String message, TYPE type) {
+        notify(message, type,
+                new NSTimestamp().timestampByAddingGregorianUnits(0, 0, 0, 0, 0, 5));
     }
-    
-    public static void displaySuccessNotification(String message) {
-        message = ERXStringUtilities.escapeJavascriptApostrophes(message);
-        AjaxUtils.javascriptResponse("alertify.success('" + message + "')",
-                ERXWOContext.currentContext());
+
+    public static void notify(String message, TYPE type, NSTimestamp expiry) {
+        NSDictionary<String, Object> guiNotification = new NSDictionary<String, Object>(
+                new Object[] { message, type, expiry, UUID.randomUUID() },
+                new String[] { "message", "type", "expiry", "guid" });
+        // for ajax requests, directly publish the notification
+        if (AjaxUtils.isAjaxRequest(ERXWOContext.currentContext().request())) {
+            AjaxUtils.javascriptResponse(alertifyNotification(guiNotification),
+                    ERXWOContext.currentContext());
+        } else {
+            // for full page loads, stash the notification on the session
+            @SuppressWarnings("unchecked")
+            NSArray<NSDictionary<String, Object>> notifications = (NSArray<NSDictionary<String, Object>>) ERXSession
+                    .session().objectStore().valueForKeyPath("CCNotifications");
+            if (notifications == null) {
+                notifications = new NSMutableArray<NSDictionary<String, Object>>();
+            } else {
+                notifications = notifications.mutableClone();
+            }
+            notifications.add(guiNotification);
+            ERXSession.session().objectStore().takeValueForKeyPath(
+                    notifications.immutableClone(), "CCNotifications");
+        }
     }
-    
-    public static void displayErrorNotification(String message) {
-        message = ERXStringUtilities.escapeJavascriptApostrophes(message);
-        AjaxUtils.javascriptResponse("alertify.error('" + message + "')",
-                ERXWOContext.currentContext());
+
+    /**
+     * @return a list of pending notifications, i.e. those that are of type
+     *         "alert" or have not expired
+     */
+    public NSArray<NSDictionary<String, Object>> pendingNotifications() {
+        NSMutableArray<NSDictionary<String, Object>> pendingNotifications = new NSMutableArray<>();
+        @SuppressWarnings("unchecked")
+        NSArray<NSDictionary<String, Object>> notifications = (NSArray<NSDictionary<String, Object>>) ERXSession
+                .session().objectStore().valueForKeyPath("CCNotifications");
+        if (notifications != null) {
+            for (NSDictionary<String, Object> aNotification : notifications) {
+                boolean isNotExpired = new NSTimestamp()
+                        .before((NSTimestamp) aNotification.valueForKey("expiry"));
+                boolean isAlert = TYPE.alert.equals(aNotification.valueForKey("type"));
+                if (isNotExpired || isAlert) {
+                    pendingNotifications.addObject(aNotification);
+                }
+            }
+            notifications = pendingNotifications.immutableClone();
+            // replace the complete notification list with the filtered one
+            ERXSession.session().objectStore().takeValueForKeyPath(notifications,
+                    "CCNotifications");
+        }
+        return notifications;
     }
-    
+
+    /**
+     * @return the script string to trigger display of a GUI notification
+     */
+    public String alertifyNotification() {
+        return alertifyNotification(aNotification);
+    }
+
+    /**
+     * @param notification
+     * @return the script string to trigger display of a GUI notification
+     */
+    private static String alertifyNotification(NSDictionary<String, Object> notification) {
+        StringBuilder alertifyNotification = new StringBuilder("alertify.");
+        if (TYPE.alert.equals(notification.valueForKey("type"))) {
+            alertifyNotification.append("alert");
+        } else if (TYPE.error.equals(notification.valueForKey("type"))) {
+            alertifyNotification.append("error");
+        } else {
+            alertifyNotification.append("success");
+        }
+        alertifyNotification.append("('");
+        alertifyNotification.append(notification.valueForKey("message"));
+        alertifyNotification.append("');");
+        return alertifyNotification.toString();
+    }
+
 }
