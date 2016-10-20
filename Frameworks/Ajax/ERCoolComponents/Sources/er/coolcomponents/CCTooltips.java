bc268e679b3aa3eb9e13b1d6bd6a940a6a801a87
Fabian Peters <fabian@mercadu.de>
2016-10-20 11:41:27 +0200

    Move tooltip feature to its own component. In spite of its commit message, the previous commit contained the opentip webserver resources.

---
+++ Frameworks/Ajax/ERCoolComponents/Sources/er/coolcomponents/CCTooltips.java
@@ -0,0 +1,15 @@
+package er.coolcomponents;
+
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.components.ERXStatelessComponent;
+
+public class CCTooltips extends ERXStatelessComponent {
+
+    private static final long serialVersionUID = 1L;
+
+    public CCTooltips(WOContext aContext) {
+        super(aContext);
+    }
+
+}
