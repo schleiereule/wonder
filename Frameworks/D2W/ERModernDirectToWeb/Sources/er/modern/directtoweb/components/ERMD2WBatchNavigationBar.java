4acf661ef8630acb0c3325f66ea82a67eb762a66
Fabian Peters <fabian@mercadu.de>
2018-04-09 14:10:30 +0200

    Implement hotkey navigation for ERMD2WBatchNavigationBar. Use batchNavNextHotkey and batchNavPreviousHotkey to define the key bindings and make sure to limit the rule to list page configurations, i.e. pages containing only a single instance of ERMD2WBatchNavigationBar.

--- Frameworks/D2W/ERModernDirectToWeb/Sources/er/modern/directtoweb/components/ERMD2WBatchNavigationBar.java
+++ Frameworks/D2W/ERModernDirectToWeb/Sources/er/modern/directtoweb/components/ERMD2WBatchNavigationBar.java
@@ -22,11 +22,18 @@ public class ERMD2WBatchNavigationBar extends AjaxFlickrBatchNavigation {
     // ACCESSORS
     
     public D2WContext d2wContext() {
+        if (_d2wContext == null) {
+            _d2wContext = (D2WContext) valueForBinding("d2wContext");
+        }
     	return _d2wContext;
     }
     
     public void setD2wContext(D2WContext c) {
     	_d2wContext = c;
+    }
+    
+    public boolean hasHotkeys() {
+        return d2wContext().valueForKey("batchNavNextHotkey") != null || d2wContext().valueForKey("batchNavPreviousHotkey") != null;
     }
     
 }
